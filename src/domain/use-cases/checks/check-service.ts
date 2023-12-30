6038e3037519b8c70be15f6af436d7e8cc5c2759
Jona <jona_jko@hotmail.com>
2023-12-29 19:42:23 -0600

    Proyecto NOC

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,36 @@
+
+interface CheckServiceUseCase {
+    execute( url: string ): Promise<boolean>;
+}
+
+type SuccessCallback = () => void;
+type ErrorCallback = ( error: string ) => void;
+
+export class CheckService implements CheckServiceUseCase {
+
+    constructor(
+        private readonly successCallback: SuccessCallback,
+        private readonly errorCallback: ErrorCallback,
+    ) {}
+    
+    async execute( url: string ): Promise<boolean> {
+
+        try {
+            const request = await fetch( url );
+
+            if( !request.ok ) {
+                throw new Error(`Error on check service${ url }`);
+            }
+
+            this.successCallback();
+            return true;
+        } catch( error ) {
+            console.log(`${ error }`);
+
+            this.errorCallback(`${error}`);
+            return false;
+        }
+        
+    }
+
+}
