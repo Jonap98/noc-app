11cdfc730148c47feeb013f675fc6c577bb189f8
Jona <jona_jko@hotmail.com>
2023-12-29 22:08:52 -0600

    Agrega capas de clean architecture

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,13 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource(),
+    // new postgreSQLLogDatasource(),
+    // new mongoLogDS(),
+);
 
 export class Server {
     public static start() {
@@ -11,6 +19,7 @@ export class Server {
                 const url = 'http://localhost:3000';
                 
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log( `${ url } is ok` ),
                     ( error ) => console.log( error )
                 ).execute( 'http://localhost:3000' );
